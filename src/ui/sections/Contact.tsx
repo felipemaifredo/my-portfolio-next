9c0c761d0057225b065ea5f59326bd137f57dd16
Felipe Maifredo <felipemaifredo@gmail.com>
2025-01-19 20:00:46 -0300

    att

--- src/ui/sections/Contact.tsx
+++ src/ui/sections/Contact.tsx
@@ -1,6 +1,6 @@
 "use client"
 //imports
-import "./Styles/contact.css"
+import "./styles/contact.css"
 import React, { useState } from "react"
 import { VscGithub } from "react-icons/vsc"
 import { AiOutlineLinkedin } from "react-icons/ai"
