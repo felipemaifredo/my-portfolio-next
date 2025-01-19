63e008a7151fc3884e60c1047db57bd2772ba1f4
Felipe Maifredo <felipemaifredo@gmail.com>
2025-01-19 20:02:42 -0300

    att

--- src/i18n/routing.ts
+++ src/i18n/routing.ts
@@ -3,11 +3,12 @@ import { createNavigation } from "next-intl/navigation"
 
 export const routing = defineRouting({
   // A list of all locales that are supported
-  locales: ["pt", "en", "es"],
+  locales: ["pt", "en",],
   // Used when no locale matches
   defaultLocale: "pt",
   pathnames: {
     "/": "/",
+    "/works": "/works",
   }
 })
 
